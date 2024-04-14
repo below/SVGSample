c0afdf30326f6039155977956571a23266e6dc1e
Alexander von Below <alex@vonbelow.com>
2024-04-14 13:30:26 +0200

    Add original image

--- SVGSample/ContentView.swift
+++ SVGSample/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("plus")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
         .padding()
     }
