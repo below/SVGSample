ecc0c6303d41ca3c6fa730b6f1faf742874d28ce
Alexander von Below <alex@vonbelow.com>
2024-04-14 13:28:56 +0200

    Initial Commit

---
+++ SVGSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SVGSample
+//
+//  Created by Alexander von Below on 14.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SVGSample/SVGSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVGSampleApp.swift
+//  SVGSample
+//
+//  Created by Alexander von Below on 14.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SVGSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
